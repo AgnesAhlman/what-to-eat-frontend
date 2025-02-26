c3b54a2b5cc71d3b0ed8adf29a03027b9f885b92
Agnes Ahlman <a71898@eon.com>
2025-02-26 21:42:17 +0100

    basic setup to fetch all recipes

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,60 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-
-const viteLogo = '/vite.svg';
-
+import { useEffect, useState } from 'react';
 import './App.css';
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from './components/ui/card';
+import { getAllRecipes } from './api/recipeApi';
+import { Recipe } from './types';
+
 function App() {
-  const [count, setCount] = useState(0);
+  const [recipes, setRecipes] = useState<Recipe[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const data = await getAllRecipes();
+        setRecipes(data);
+      } catch (error) {
+        if (error instanceof Error) {
+          setError(error.message);
+        } else {
+          setError('An unknown error occurred');
+        }
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error: {error}</div>;
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {recipes.map((recipe) => (
+        <Card key={recipe.id}>
+          <CardHeader>
+            <CardTitle>{recipe.title}</CardTitle>
+            <CardDescription>{recipe.description}</CardDescription>
+          </CardHeader>
+          <CardContent></CardContent>
+          <CardFooter></CardFooter>
+        </Card>
+      ))}
     </>
   );
 }
